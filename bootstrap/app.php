e580f6402a593c84e9bc950dca717bcaab7b5425
Traore Issouf <i.traore@agitex.fr>
2024-07-31 11:22:45 +0000

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,8 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
     apiPrefix: '/api',
   )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        
     })->create();
